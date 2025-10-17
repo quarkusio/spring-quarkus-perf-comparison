ac9f09e1ff0aba73eeb545f3e32531961391e6a7
Holly Cummins <holly.k.cummins@googlemail.com>
2025-10-17 19:08:26 +0100

    Fix jar paths

--- scripts/1strequest.sh
+++ scripts/1strequest.sh
@@ -8,10 +8,10 @@
 # $ ./1strequest.sh "java -XX:ActiveProcessorCount=8 -Xms512m -Xmx512m -jar ../springboot3/target/springboot3.jar" 10
 #
 # 2) Run the Quarkus app 10 times
-# $ ./1strequest.sh "java -XX:ActiveProcessorCount=8 -Xms512m -Xmx512m -jar ../quarkus3/target/quarkus-app/quarkus3.jar" 10
+# $ ./1strequest.sh "java -XX:ActiveProcessorCount=8 -Xms512m -Xmx512m -jar ../quarkus3/target/quarkus-app/quarkus-run.jar" 10
 #
 # 3) Run the Quarkus with spring compatibility app 10 times
-# $ ./1strequest.sh "java -XX:ActiveProcessorCount=8 -Xms512m -Xmx512m -jar ../quarkus3-spring-compatibility/target/quarkus-app/app.jar" 10
+# $ ./1strequest.sh "java -XX:ActiveProcessorCount=8 -Xms512m -Xmx512m -jar ../quarkus3-spring-compatibility/target/quarkus-app/quarkus-run.jar" 10
 
 COMMAND=$1
 NUM_ITERATIONS=1
