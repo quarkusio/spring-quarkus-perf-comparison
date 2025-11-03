0c354438eb8c0db3bb33a7274c72a9a9c096532a
Holly Cummins <holly.k.cummins@googlemail.com>
2025-11-03 21:53:14 +0000

    Allow script to be called from other directories by prepending relative directory more consistently

--- scripts/stress.sh
+++ scripts/stress.sh
@@ -8,7 +8,7 @@ kill $(lsof -t -i:8080) &>/dev/null
 set -euo pipefail
 
 ${thisdir}/infra.sh -s
-java -XX:ActiveProcessorCount=8 -Xms512m -Xmx512m -jar $1 &
+java -XX:ActiveProcessorCount=4 -Xms512m -Xmx512m -jar ${thisdir}/../$1 &
 jbang wrk@hyperfoil -t2 -c100 -d20s --timeout 1s http://localhost:8080/fruits
 ${thisdir}/infra.sh -d
 kill $(lsof -t -i:8080) &>/dev/null
