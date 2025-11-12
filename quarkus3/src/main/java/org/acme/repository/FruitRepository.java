05632c546bb8d2581d65b8d9affc971f40bca3cc
Eric Deandrea <eric.deandrea@ibm.com>
2025-11-12 09:35:42 +0100

    Use PanacheRepository instead of PanacheRepositoryBase

--- quarkus3/src/main/java/org/acme/repository/FruitRepository.java
+++ quarkus3/src/main/java/org/acme/repository/FruitRepository.java
@@ -2,14 +2,14 @@ package org.acme.repository;
 
 import java.util.Optional;
 
-import org.acme.domain.Fruit;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
-
 import jakarta.enterprise.context.ApplicationScoped;
 
+import org.acme.domain.Fruit;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
 @ApplicationScoped
-public class FruitRepository implements PanacheRepositoryBase<Fruit, Long> {
+public class FruitRepository implements PanacheRepository<Fruit> {
 	public Optional<Fruit> findByName(String name) {
 		return find("name", name).firstResultOptional();
 	}
