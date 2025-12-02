9e94a2617847de18d9d4bae1ffb3e27b74ef0cd4
Eric Deandrea <eric.deandrea@ibm.com>
2025-12-02 17:38:36 -0500

    Fix Spring Boot 4 native

--- springboot4/src/main/java/org/acme/SpringBoot4Application.java
+++ springboot4/src/main/java/org/acme/SpringBoot4Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(L2CacheRuntimeHints.class)
+@ImportRuntimeHints({ HibernateNativeHints.class, L2CacheRuntimeHints.class })
 public class SpringBoot4Application {
 
 	public static void main(String[] args) {
