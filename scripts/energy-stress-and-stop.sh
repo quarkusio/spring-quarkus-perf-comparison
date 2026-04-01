d90d669fff9e8a4abe3a9c5909f624e53f910975
Holly Cummins <holly.k.cummins@googlemail.com>
2026-04-01 11:23:36 +0200

    Bold the latency line

--- scripts/energy-stress-and-stop.sh
+++ scripts/energy-stress-and-stop.sh
@@ -3,6 +3,15 @@
 
 thisdir=`dirname "$0"`
 
-jbang wrk2@hyperfoil -t2 -c100 -d20s --rate 2000 --timeout 1s http://localhost:8080/fruits
+# Run wrk2 and format output to emphasise the latency
+jbang wrk2@hyperfoil -t2 -c100 -d20s --rate 2000 --timeout 1s http://localhost:8080/fruits | while IFS= read -r line; do
+    # Make only the "Latency" summary line bold (contains average latency)
+    if [[ "$line" =~ ^[[:space:]]*Latency[[:space:]] ]]; then
+        echo -e "\033[1m${line}\033[0m"
+    else
+        echo "$line"
+    fi
+done
+
 kill $(lsof -t -i:8080) &>/dev/null
 ${thisdir}/infra.sh -d
